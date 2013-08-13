20bf69e693894ae6d8f339a63759748c39cb039c
y_jono <pek9dkl@gmail.com>
2013-08-14 01:35:34 +0900

    Viewファイルの中身を読みやすいようにリファクタリングした。
    画面に出力するためにhtmlエンコーディングが必要な箇所wo
    htmlspecialchars()を使い修正した。
    <?php ?>タグ内のセミコロンを省略した。
    <?php ?>タグを <? ?>に修正した。
    <?php echo hoge ?>タグを <?= hoge ?>に修正した。

--- index.php
+++ index.php
@@ -1,77 +1,83 @@
 <?php
-    include_once 'get_all_task.inc';
+include_once 'get_all_task.inc';
+
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
+
 ?>
 
 <html lang="ja">
-  <head>
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="Content-Language" content="ja">
     <meta http-equiv="Content-Script-Type" content="text/javascript">
     <meta http-equiv="Content-Style-Type" content="text/css">
     <title>Shared Tasks</title>
     <meta name="robots" content="noindex">
-      <!--
-    <style type="text/css">
-      * {
-        margin: 0;
-        padding: 0;
-        font-size: 100%;
-        font-weight: normal;
-      }
-      
-      li {
-        list-style-type: none;
-      }
-      
-      img {
-        vertical-align: bottom;
-      }
-      
-      a img {
-        border: none;
-      }
-      
-      body {
-        padding-top: 100px;
-        width: 100%;
-        line-height: 1.2;
-        background: #346;
-        color: #fff;
-        font-size: 80%;
-      }
-      
-      #mainList {
-        margin-top: 50px;
-      }
-    </style>
-    -->
+    <!--
+  <style type="text/css">
+    * {
+      margin: 0;
+      padding: 0;
+      font-size: 100%;
+      font-weight: normal;
+    }
+
+    li {
+      list-style-type: none;
+    }
+
+    img {
+      vertical-align: bottom;
+    }
+
+    a img {
+      border: none;
+    }
+
+    body {
+      padding-top: 100px;
+      width: 100%;
+      line-height: 1.2;
+      background: #346;
+      color: #fff;
+      font-size: 80%;
+    }
+
+    #mainList {
+      margin-top: 50px;
+    }
+  </style>
+  -->
     <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="/script.js"></script>
-    
-  </head>
-  <body>
-  <form  action="./" method="post">
+
+</head>
+<body>
+<form action="./" method="post">
     <p>
-      追加する項目:<input type="text" width="100" name="name" value="">
-      担当:
-        <select name="person">
-          <option value="0">せいの</option>
-          <option value="1">にしむら</option>
+        <label for="newtaskname">追加する項目:</label><input type="text" width="100" name="name" value="" id="newtaskname">
+        <label for="newtaskowner">担当:</label>
+        <select name="person" id="newtaskowner">
+            <option value="0">せいの</option>
+            <option value="1">にしむら</option>
         </select>
-      <input type="submit" class="btn" value="登録">
+        <input type="submit" class="btn" value="登録">
     </p>
-  </form>
-  <ul id="mainList">
-  <?php foreach($result_set as $row1): ?>
-      <li>
-          <input type="checkbox" class="task"<?php if($row1['checked'] == 1): ?> checked <?php endif; ?> name="<?php echo $row1['id']; ?>" value="1">
-          <?php print($row1['name']); ?>（<?php echo $row1['person']; ?>）
-          <input type="submit" value="削除" class="delete btn" name="<?php echo $row1['id']; ?>">
-      </li>
-  <?php endforeach; ?>
-  </ul>
-  <p style="margin-top:30px">※タスクのチェックと削除ができるようになりました※</p>
-  </body>
+</form>
+<ul id="mainList">
+    <?php foreach ($result_set as $row1): ?>
+        <li>
+            <input type="checkbox" class="task"<?php if ($row1['checked'] == 1): ?> checked <?php endif ?>
+                   name="<?= $row1['id'] ?>" value="1">
+            <?= h($row1['name']) ?>（<?= h($row1['person']) ?>）
+            <input type="submit" value="削除" class="delete btn" name="<?= $row1['id'] ?>">
+        </li>
+    <?php endforeach ?>
+</ul>
+<p style="margin-top:30px">※タスクのチェックと削除ができるようになりました※</p>
+</body>
 </html>
